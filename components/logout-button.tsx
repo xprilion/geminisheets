85b64768af9d552f0c43f73f80e3b7ba89df5c87
xprilion <xprilion@gmail.com>
2024-02-29 23:48:49 +0530

    push again due to wrong push earlier

---
+++ components/logout-button.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import * as React from "react"
+
+import { Button } from "@/components"
+import {signOut} from "next-auth/react";
+
+
+export function LogoutButton() {
+
+    return (
+        <div>
+            <Button color="white" variant="outlined" size="md" ripple={true} placeholder={"Logout"}  onClick={() => signOut()}>
+                Logout
+            </Button>
+        </div>
+    )
+}
