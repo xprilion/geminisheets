9539b983c0ebcf1a57c4aece8639e5a018945626
xprilion <xprilion@gmail.com>
2024-03-01 00:07:46 +0530

    added some page boilerplates for sake of build pass

---
+++ app/functions/[functionId]/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from 'next';
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "@/lib/auth";
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+    title: "X Function | Gemini Sheets",
+    description: "Supercharge your Google Sheets with Gemini AI powered custom fuctions",
+  };
+  
+  export default async function Home() {
+    const session = await getServerSession(authOptions);
+
+    if(!session?.user){
+        redirect("/")
+    }
+  
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      </div>
+    );
+  }
+  
